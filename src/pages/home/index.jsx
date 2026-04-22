27be0723128058972996f221459b85707025ac89
felipehtt <fs.oliveira0014@gmail.com>
2026-04-22 17:11:23 -0300

    home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,8 +11,28 @@ export default function Home() {
 
       <Cabecalho />
 
+      <div className='conteiner'>
+
+        <div className="cont1">
+
+          <h1>CMD IA Corporation</h1>
+
+          <p>Encontre as últimas tendências em moda e estilo aqui. Explore nossa coleção de roupas para todas as ocasiões e estilos. Compre agora e atualize seu guarda-roupa com peças incríveis!</p>
+
+          <div>
+
+            <Link className='btn-comecar' to="/cmdia">Saiba mais sobre a CMD IA</Link>
+
+          </div>
+
+        </div>
+
+          <img src="../../assets/images/img-logo.png" alt="" />
+
+      </div>
+
     </div>
 
   );
-  
+
 }
